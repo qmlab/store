29380b38656e8f1ff897d76c53417099962ca94d
maine2014 <mainchin@msn.com>
2014-12-10 22:14:07 -0800

    updated db name and coll name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var dao = require('./dao.js')
 
-var dburl = 'mongodb://127.0.0.1:27017/test'
-var coll = 'store'
+var dburl = 'mongodb://127.0.0.1:27017/store'
+var coll = 'objs'
 var doc1 = {name:"david", title:"About MongoDB"};
 var doc2 = {name:"maine", title:"abc"}
 
